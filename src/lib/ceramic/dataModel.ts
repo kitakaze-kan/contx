c35bc9ba1f1d3fff5e4a59360647dab678aaae34
kitakaze-kan <kantaro.1013.fujimori@gmail.com>
2022-06-25 09:33:27 -0400

    add ceramic stuffs

---
+++ src/lib/ceramic/dataModel.ts
@@ -0,0 +1,5 @@
+import type { ModelTypes } from "@/interfaces/workcredential";
+import type { ModelTypesToAliases } from "@glazed/types";
+import { aliases as ModelAliases } from "../../__generated__/aliases";
+
+export const cVoxelModel: ModelTypesToAliases<ModelTypes> = ModelAliases;
