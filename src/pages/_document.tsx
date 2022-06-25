fd3bc4be12adaffe73b7066478e153b2ae969fd2
kitakaze-kan <kantaro.1013.fujimori@gmail.com>
2022-06-25 05:29:29 -0400

    modified tailwind

---
+++ src/pages/_document.tsx
@@ -0,0 +1,26 @@
+import Document, {
+    Html,
+    Head,
+    Main,
+    NextScript,
+  } from 'next/document'
+  
+  export default class MyDocument extends Document {
+  
+    render() {
+      return (
+        <Html lang="en">
+          <Head>
+            <link
+                href="https://fonts.googleapis.com/css2?family=Saira:wght@500&display=swap"
+                rel="stylesheet"
+            />
+            </Head>
+          <body>
+            <Main />
+            <NextScript />
+          </body>
+        </Html>
+      )
+    }
+  }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,10 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+
 
 const Home: NextPage = () => {
+  
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>Index</div>
   )
 }
 
