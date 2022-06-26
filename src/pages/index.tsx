4b0d5f63dcdb3bd8c59d7be2837285464acb6505
kitakaze-kan <kantaro.1013.fujimori@gmail.com>
2022-06-26 06:30:08 -0400

    add lp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,27 @@
 import type { NextPage } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Button } from 'react-daisyui'
 
 
 const Home: NextPage = () => {
   
   return (
-    <div>Index</div>
+    <main className="text-white text-center h-screen overflow-hidden flex justify-center ">
+            <div className="w-full text-center pt-20">
+            <div className="relative text-center mx-auto max-w-3xl h-80">
+                    <Image
+                        src="/banner.png"
+                        alt="contx"
+                        objectFit="cover"
+                        layout={"fill"}
+                    />
+              </div>
+              <Link href="/pay" className="">
+                <Button type="button" className="my-24 px-10 py-3 text-xl text-white bg-gradient-to-r from-border_l via-border_via to-border_r">Transfer</Button>
+              </Link>
+            </div>
+        </main>
   )
 }
 
